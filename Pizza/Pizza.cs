a90912fd8cd5269d2197b02f0e8918fc014dc5fe
Nick <n.vancuijk@hotmail.nl>
2018-09-29 12:02:33 +0200

    Pizza

---
+++ Pizza/Pizza.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pizza
+{
+    public class Pizza
+    {
+        public List<string> Toppings { get; set; }
+
+        public Pizza(string[] toppings)
+        {
+            Toppings = new List<string>(toppings);
+        }
+    }
+}
---
+++ Pizza/Program.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pizza
+{
+    public class Program
+    {
+        private static List<Pizza> pizzas = new List<Pizza>();
+        static void Main(string[] args)
+        {
+            using (StreamReader r = new StreamReader("pizzas.json"))
+            {
+                string json = r.ReadToEnd();
+                pizzas = JsonConvert.DeserializeObject<List<Pizza>>(json);
+            }
+
+            var orderedConfigs = pizzas.Select(x => string.Join(",", x.Toppings.OrderBy(y => y)));
+            var grouped = orderedConfigs.GroupBy(x => x);
+            Dictionary<string, int> dToppingCounts = new Dictionary<string, int>();
+
+            foreach (var g in grouped)
+            {
+                dToppingCounts.Add(g.Key, g.Count());
+            }
+
+            var orderedToppingCounts = dToppingCounts.OrderByDescending(x => x.Value);
+            var top20ToppingCounts = orderedToppingCounts.Take(20);
+
+            foreach (var t in top20ToppingCounts)
+            {
+                var topping = t.Key;
+                Console.WriteLine("Topping : " + string.Join(",", topping) + "\t\t Frequency : " + t.Value);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
